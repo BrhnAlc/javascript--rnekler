3448772c03baf2e6e90fe883dab83dd9070c09e3
burhan <alacaburhan122016@gmail.com>
2023-06-20 23:36:39 +0300

    splice

--- app.js
+++ app.js
@@ -43,11 +43,29 @@ console.log(fruits); // ['elma', 'muz', 'portakal', 'çilek']
 fruits.push('kiraz', 'üzüm');
 console.log(fruits); // ['elma', 'muz', 'portakal', 'çilek', 'kiraz', 'üzüm']
 
-//***pop */JavaScript'de pop() yöntemi, bir diziden en son öğeyi çıkarmak için kullanılan bir dizi yöntemidir. Bu yöntem, mevcut diziden son öğeyi kaldırarak dizinin boyutunu azaltır.
+//***pop *****/JavaScript'de pop() yöntemi, bir diziden en son öğeyi çıkarmak için kullanılan bir dizi yöntemidir. Bu yöntem, mevcut diziden son öğeyi kaldırarak dizinin boyutunu azaltır.
 
-let fruits = ['elma', 'muz', 'portakal'];
+let fruits2 = ['elma', 'muz', 'portakal'];
 console.log(fruits); // ['elma', 'muz', 'portakal']
 
 let lastFruit = fruits.pop();
 console.log(lastFruit); // 'portakal'
 console.log(fruits); // ['elma', 'muz']
+
+
+//* splice******JavaScript'de splice() yöntemi, bir diziden belirli bir konumda bulunan öğeleri çıkarmak veya yeni öğeler eklemek için kullanılan bir dizi yöntemidir. Bu yöntem, mevcut diziyi değiştirir ve çıkartılan veya eklenen öğeleri döndürür
+
+let fruits3 = ['elma', 'muz', 'portakal', 'çilek'];
+console.log(fruits); // ['elma', 'muz', 'portakal', 'çilek']
+
+let removedItems1 = fruits.splice(1, 2);
+console.log(removedItems); // ['muz', 'portakal']
+console.log(fruits); // ['elma', 'çilek']
+
+
+let fruits = ['elma', 'muz', 'portakal'];
+console.log(fruits); // ['elma', 'muz', 'portakal']
+
+let removedItems = fruits.splice(1, 2, 'çilek', 'kiraz');
+console.log(removedItems); // ['muz', 'portakal']
+console.log(fruits); // ['elma', 'çilek', 'kiraz']
