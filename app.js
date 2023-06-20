ef75c600ef08436900f5a790d676317d69217f73
burhan <alacaburhan122016@gmail.com>
2023-06-20 23:26:04 +0300

    push

---
+++ app.js
@@ -0,0 +1,53 @@
+   //*  variables
+
+let name ="burhan alaca";
+console.log(name);
+name="ahmet";
+console.log(name);
+
+
+
+const isim ="burhanın arabası";
+console.log(isim);
+// isim="ahmet arabası";
+console.log(isim);
+
+
+let number=50;
+console.log(number);
+
+number=number +10
+number=number*12
+number=number%11
+
+console.log(number);
+
+
+
+//!***** array******
+
+const isimler =["mehmet", "ahmet","mustafa","murat"];
+console.log( isimler.length);
+console.log(isimler);
+console.log(typeof isimler);
+// isimler.push(3);
+
+//* **push***** :javaScript'de push() yöntemi, bir dizinin sonuna bir veya daha fazla öğe eklemek için kullanılan bir dizi yöntemidir. Bu yöntem, mevcut diziye yeni öğeler ekleyerek dizinin boyutunu değiştirir
+
+let fruits1 = ['elma', 'muz', 'portakal'];
+console.log(fruits); // ['elma', 'muz', 'portakal']
+
+fruits.push('çilek');
+console.log(fruits); // ['elma', 'muz', 'portakal', 'çilek']
+
+fruits.push('kiraz', 'üzüm');
+console.log(fruits); // ['elma', 'muz', 'portakal', 'çilek', 'kiraz', 'üzüm']
+
+//***pop */JavaScript'de pop() yöntemi, bir diziden en son öğeyi çıkarmak için kullanılan bir dizi yöntemidir. Bu yöntem, mevcut diziden son öğeyi kaldırarak dizinin boyutunu azaltır.
+
+let fruits = ['elma', 'muz', 'portakal'];
+console.log(fruits); // ['elma', 'muz', 'portakal']
+
+let lastFruit = fruits.pop();
+console.log(lastFruit); // 'portakal'
+console.log(fruits); // ['elma', 'muz']
